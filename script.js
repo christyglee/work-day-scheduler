78c799996cf45930ee898c7e870d2db307da0490
Christy Lee <christy.g.lee@gmail.com>
2020-09-26 19:33:44 -0700

    worked on setting current time to textarea with time difference calc. and got local storage to work

--- script.js
+++ script.js
@@ -17,35 +17,84 @@ $(document).ready(function(){
     // console.log(timeBlock);
 
     var currentTime = moment().format('HH'); 
+    //  moment().hours(currentHour).minutes(0).seconds(0)
+
     // console.log(currentTime);
 
     var currentHour = parseInt(timeBlock.slice(0, timeBlock.length - 2));
     if(currentHour >= 1 && currentHour <= 5){
     currentHour += 12;
     }
-    if(currentHour < currentTime) {
-    } //Past current time - need to edit colors in css
-    else if(currentHour > currentTime) {
-    } //Future current time - need to edit colors in css
-    else {
-    } //Present time - need to edit colors in css
-    console.log(currentHour);
+    }
+    // if(currentHour < currentTime) {
+    // } //Past current time - need to edit colors in css
+    // else if(currentHour > currentTime) {
+    // } //Future current time - need to edit colors in css
+    // else {
+    // } //Present time - need to edit colors in css
+    // console.log(currentHour);
 
 // Need to somehow link the time to middle textarea column to correspond with current time
     var textHour = $(".textHour");
-    textHour = currentHour;
+    // console.log(textHour.val(currentHour));
     // console.log(textHour);
+    var realTime = moment().hours(currentHour).minutes(0).seconds(0);
+    
+    var timeDiff = (realTime.diff(moment(),"minutes"));
+    console.log(timeDiff);
     if(textHour  >= 1 && textHour <= 5){
         textHour += 12;
         }
-        if(textHour < currentTime) {
+
+    $.each(textHour,function(){
+        if(timeDiff < -60) {
+            textHour.css("background-color", "#d3d3d3");
+
         } //Past current time - need to edit colors in css
-        else if(textHour > currentTime) {
+        else if(timeDiff > 0) {
+            textHour.css("background-color", "#77dd77");
         } //Future current time - need to edit colors in css
         else {
+            textHour.css("background-color", "#ff6961");
         } //Present time - need to edit colors in css
-        console.log(textHour);
+        // console.log(textHour);
+    });
     
+
+    
+
+    // }
+    // Local storage
+
+    
+
+    var textarea = $(".textHour");
+    
+    var saveBtn = $(".saveBtn");
+
+    saveBtn.on("click", function(){
+        var textInput = $(this).prev().val();
+        var storageKey = $(this).parent().find(".hour").text();
+
+        localStorage.setItem(storageKey,textInput);
+    });
+
+    $("textarea").each(function(){
+        var storageKey = $(this).prev().text();
+
+        var retrieveItem = localStorage.getItem(storageKey);
+
+        $(this).val(retrieveItem);
+
+    })
+
+
+
+    });
+
+    //
+    
+
 
     // var textHour = $(".textHour");
     // textHour = hour
@@ -58,12 +107,9 @@ $(document).ready(function(){
     //     $(".textHour").text($("input").val());
 
     // })
+    
 
 
-
-
-}
-});
 
 
 
